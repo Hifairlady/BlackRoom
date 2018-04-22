89b50485cdf332619bade481de6b9ba52e4cbcd0
Anderson <1452192394@qq.com>
2018-04-22 15:40:53 +0800

    Perfect HtmlTextView

--- app/src/main/java/com/edgar/blackroom/Activities/BulletinDetailActivity.java
+++ app/src/main/java/com/edgar/blackroom/Activities/BulletinDetailActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -28,18 +29,15 @@ public class BulletinDetailActivity extends AppCompatActivity {
     private static final int GET_DATA_SUCCESS = 101;
     private static final int GET_DATA_FAILED = 102;
 
-    private String appbarTitleString = "";
     private LinearLayout bulletinContainer;
     private TextView tvTitle, tvDate;
     private HtmlTextView htmlTextView;
 
-    private Handler getContentHandler;
     private ParseHtmlThread parseThread;
-    private String queryUrl;
 
     private String titleString, dateString;
-    private String[] htmlCodeStrings;
     private String htmlCodeString;
+
     private ParseHtmlThread.DataFromDocument dataFromDocument = new ParseHtmlThread.DataFromDocument() {
         @Override
         public void setData(Document document) {
@@ -53,13 +51,11 @@ public class BulletinDetailActivity extends AppCompatActivity {
 
             Elements paragraphElements = textContentElement.children();
 
-            htmlCodeString = paragraphElements.html();
-//            Log.d(TAG, "setData: " + htmlCodeString);
+            htmlCodeString = "";
 
-            htmlCodeStrings = new String[paragraphElements.size()];
-            int index = 0;
             for (Element element : paragraphElements) {
-                htmlCodeStrings[index++] = element.html();
+                htmlCodeString = htmlCodeString.concat(element.html() + "<br />");
+                Log.d(TAG, "setData: " + htmlCodeString);
             }
 
         }
@@ -77,8 +73,8 @@ public class BulletinDetailActivity extends AppCompatActivity {
     private void initUIData() {
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
-        appbarTitleString = getIntent().getStringExtra("titleString");
-        queryUrl = getIntent().getStringExtra("queryUrl");
+        String appbarTitleString = getIntent().getStringExtra("titleString");
+        String queryUrl = getIntent().getStringExtra("queryUrl");
         toolbar.setTitle(appbarTitleString);
         toolbar.setTitleTextAppearance(this, R.style.BulletinDetailTitle);
         setSupportActionBar(toolbar);
@@ -95,10 +91,10 @@ public class BulletinDetailActivity extends AppCompatActivity {
         bulletinContainer = findViewById(R.id.official_bulletin_container);
         tvDate = findViewById(R.id.official_publish_date);
         tvTitle = findViewById(R.id.official_bulletin_title);
-//        htmlTextView = (HtmlTextView)findViewById(R.id.my_html_text_view);
-//        htmlTextView.setRemoveFromHtmlSpace(false);
+        htmlTextView = findViewById(R.id.my_html_text_view);
+        htmlTextView.setRemoveFromHtmlSpace(true);
 
-        getContentHandler = new Handler() {
+        Handler getContentHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -108,20 +104,9 @@ public class BulletinDetailActivity extends AppCompatActivity {
                         tvTitle.setText(titleString);
                         tvDate.setText(dateString);
 
-                        for (int i = 0; i < htmlCodeStrings.length; i++) {
-                            HtmlTextView tvHtml = new HtmlTextView(getBaseContext());
-                            HtmlHttpImageGetter imageGetter = new HtmlHttpImageGetter(tvHtml,
-                                    null, true);
-                            tvHtml.setTextSize(16.f);
-                            tvHtml.setTextColor(getColor(R.color.primary_text));
-                            tvHtml.setLinkTextColor(getColor(R.color.colorPrimary));
-                            tvHtml.setHtml(htmlCodeStrings[i], imageGetter);
-                            bulletinContainer.addView(tvHtml);
-                        }
-
-//                        HtmlHttpImageGetter imageGetter = new HtmlHttpImageGetter(htmlTextView,
-//                                null, true);
-//                        htmlTextView.setHtml(htmlCodeString, imageGetter);
+                        HtmlHttpImageGetter imageGetter = new HtmlHttpImageGetter(htmlTextView,
+                                null, true);
+                        htmlTextView.setHtml(htmlCodeString, imageGetter);
 
                         Snackbar.make(bulletinContainer, parseThread.getResultString(),
                                 Snackbar.LENGTH_SHORT).show();
